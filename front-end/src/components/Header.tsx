064b2d446acc5ac0a06daca4cc9e0ae8dfc1b6fb
Guilhermefonseca2021 <gsbloogs194@gmail.com>
2024-09-09 10:27:17 -0300

    simple temaple not treating api

---
+++ front-end/src/components/Header.tsx
@@ -0,0 +1,10 @@
+export default function Header() {
+  return (
+    <div className="flex justify-between p-4 bg-[#9333EA] text-slate-100 text-3xl">
+      <h1>Quiz</h1>
+      <ul>
+        <li >User</li>
+      </ul>
+    </div>
+  )
+}
---
+++ front-end/src/components/QuizSection.tsx
@@ -0,0 +1,10 @@
+
+export default function QuizSection() {
+  // const [quiz, setQuiz] = useState<Promise<typeof api>>();
+  // console.log(api)
+
+  return (
+    <div className="grid grid-cols-2 gap-4">
+    </div>
+  );
+}
